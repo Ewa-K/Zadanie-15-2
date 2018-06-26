b5fed7276e04f51df76bc1928ab3c4dd9dcb0a8c
Ewa-K <ewa-1988@yahoo.com>
2018-06-26 14:29:14 +0200

    Change project

--- components/App.js
+++ components/App.js
@@ -15,13 +15,17 @@ App = React.createClass({
         this.setState({
             loading: true
         });
-        this.getGif(searchingText, function(gif) {
-            this.setState({
-                loading: false,
-                gif: gif,
-                searchingText: searchingText
-            });
-        }.bind(this));
+        this.getGif(searchingText)
+            .then(function(gif) {
+                this.setState({
+                    loading: false,
+                    gif: gif,
+                    searchingText: searchingText
+                });
+        }.bind(this))
+        .catch(function(err) {
+            console.log(err)
+        })
     },
 
     getGif: function(searchingText, callback) {
@@ -37,35 +41,15 @@ App = React.createClass({
                             url: data.fixed_width_downsampled_url,
                             sourceUrl: data.url
                         };
-                        callback(gif);
-                        resolve(this.response);
+                        resolve(gif);
                     }
                 } else {
-                    reject(error);
+                    reject(JSON.parse(xhr.responseText));
                 }
-            });
-        xhr.send();
-        .then(response => console.log('Contents: ' + response))
-        .catch(error => console.error('Something went wrong', error));
-    },
-    
-
-    /*getGif: function(searchingText, callback) {
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data;
-                var gif = {
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);
             }
-        };
-        xhr.send();
-    },*/
+            xhr.onerror = reject
+            xhr.send();
+    }),
 
     render: function() {
         var styles = {
